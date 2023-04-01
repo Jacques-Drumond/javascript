33be9219570fd73b4520601ea13e43780e9102dc
Jacques-Drumond <jacques.junio@hotmail.com>
2023-04-01 10:15:45 -0300

    a

--- condingchall.js
+++ condingchall.js
@@ -1,370 +1,407 @@
-/* c
-onsole.log("Coding challenge");
-// test data 1
-console.log("First data");
-let marksWeight = 78;
-let marksHeight = 1.69;
+// // onsole.log("Coding challenge");
+// // // test data 1
+// // console.log("First data");
+// // let marksWeight = 78;
+// // let marksHeight = 1.69;
 
-let johnsWeight = 92;
-let johnHeight = 1.95;
+// // let johnsWeight = 92;
+// // let johnHeight = 1.95;
 
-let marksBMI = marksWeight / marksHeight **2
-let johnsBMI = johnsWeight / johnHeight **2
-console.log(marksBMI)
-console.log(johnsBMI)
-let markHigherBMI = marksBMI > johnsBMI
-console.log(markHigherBMI)
+// // let marksBMI = marksWeight / marksHeight **2
+// // let johnsBMI = johnsWeight / johnHeight **2
+// // console.log(marksBMI)
+// // console.log(johnsBMI)
+// // let markHigherBMI = marksBMI > johnsBMI
+// // console.log(markHigherBMI)
 
-/* test data 2
-console.log("Second data");
+// //  test data 2
+// // console.log("Second data");
 
-let marksWeight2 = 95;
-let marksHeight2 = 1.88;
+// // let marksWeight2 = 95;
+// // let marksHeight2 = 1.88;
 
-let johnsWeight2 = 85;
-let johnHeight2 = 1.76;
+// // let johnsWeight2 = 85;
+// // let johnHeight2 = 1.76;
 
-let marksBMI2 = marksWeight2 / marksHeight2 **2
-let johnsBMI2 = johnsWeight2 / johnHeight2 **2
-console.log(marksBMI2)
-console.log(johnsBMI2)
-let markHigherBMI2 = marksBMI2 > johnsBMI2
-console.log(markHigherBMI2)
+// // let marksBMI2 = marksWeight2 / marksHeight2 **2
+// // let johnsBMI2 = johnsWeight2 / johnHeight2 **2
+// // console.log(marksBMI2)
+// // console.log(johnsBMI2)
+// // let markHigherBMI2 = marksBMI2 > johnsBMI2
+// // console.log(markHigherBMI2)
 
-const firstName = "Jacques"
-const job = "programmer"
-const birthYear = 2002
-const year = 2023
+// // const firstName = "Jacques"
+// // const job = "programmer"
+// // const birthYear = 2002
+// // const year = 2023
 
-const Jacques = "I'm " + firstName + ', a ' + (year - birthYear) + ' years old ' + job
+// // const Jacques = "I'm " + firstName + ', a ' + (year - birthYear) + ' years old ' + job
 
-console.log(Jacques)
+// // console.log(Jacques)
 
-const JacquesNew = `I'm ${firstName}, a ${year - birthYear} years old ${job}`
+// // const JacquesNew = `I'm ${firstName}, a ${year - birthYear} years old ${job}`
 
-console.log(JacquesNew) 
+// // console.log(JacquesNew)
 
-let age = 19;
+// // let age = 19;
 
-if (age >= 18) {
-  console.log(`You can drive 😁 🚗`);
-} else {
-  let yearsLeft = 18 - age;
-  console.log(`You can't drive 😕 🚗 \n${yearsLeft} years left`);
-}
+// // if (age >= 18) {
+// //   console.log(`You can drive 😁 🚗`);
+// // } else {
+// //   let yearsLeft = 18 - age;
+// //   console.log(`You can't drive 😕 🚗 \n${yearsLeft} years left`);
+// // }
 
-let birthYear = 1999;
+// // let birthYear = 1999;
 
-let century;
-let end;
+// // let century;
+// // let end;
 
-if (birthYear <= 2000) {
-  century = 20;
-  end = "th";
-} else {
-  century = 21;
-  end = "st";
-}
+// // if (birthYear <= 2000) {
+// //   century = 20;
+// //   end = "th";
+// // } else {
+// //   century = 21;
+// //   end = "st";
+// // }
 
-console.log(`Person was born in the ${century}${end} century`);
+// // console.log(`Person was born in the ${century}${end} century`);
 
+// // // challenge 2
 
-// challenge 2
+// // console.log("challenge 2");
+// // let marksWeight = 78;
+// // let marksHeight = 1.69;
+// // let johnsWeight = 92;
+// // let johnHeight = 1.95;
 
-console.log("challenge 2");
-let marksWeight = 78;
-let marksHeight = 1.69;
-let johnsWeight = 92;
-let johnHeight = 1.95;
+// // let marksBMI = marksWeight / marksHeight **2
+// // let johnsBMI = johnsWeight / johnHeight **2
 
-let marksBMI = marksWeight / marksHeight **2
-let johnsBMI = johnsWeight / johnHeight **2
+// // let marksWeight2 = 95;
+// // let marksHeight2 = 1.88;
+// // let johnsWeight2 = 85;
+// // let johnHeight2 = 1.76;
 
+// // let marksBMI2 = marksWeight2 / marksHeight2 **2
+// // let johnsBMI2 = johnsWeight2 / johnHeight2 **2
 
+// // if (marksBMI > johnsBMI){
+// //     console.log(`Mark's BMI (${marksBMI}) is higher than John's BMI (${johnsBMI})!`)
+// // } else {
+// //     console.log(`John's BMI (${johnsBMI}) is higher than Mark's BMI (${marksBMI})!`)
+// // }
 
-let marksWeight2 = 95;
-let marksHeight2 = 1.88;
-let johnsWeight2 = 85;
-let johnHeight2 = 1.76;
+// // let inputYear = "2002";
+// // console.log(typeof inputYear);
 
-let marksBMI2 = marksWeight2 / marksHeight2 **2
-let johnsBMI2 = johnsWeight2 / johnHeight2 **2
+// // inputYear = Number(inputYear);
 
+// // console.log(inputYear + 18);
 
-
-
-if (marksBMI > johnsBMI){
-    console.log(`Mark's BMI (${marksBMI}) is higher than John's BMI (${johnsBMI})!`)
-} else {
-    console.log(`John's BMI (${johnsBMI}) is higher than Mark's BMI (${marksBMI})!`)
-}
-
-let inputYear = "2002";
-console.log(typeof inputYear);
-
-inputYear = Number(inputYear);
-
-console.log(inputYear + 18);
-
-console.log(typeof inputYear);
-
-
-let inputNumber = "2002";
-
-inputNumber = Number(inputNumber);
-
-console.log(20223 - inputNumber);
-
-
-console.log(Boolean(0));
-console.log(Boolean(9));
-console.log(Boolean("jacques"));
-console.log(Boolean(true));
-console.log(Boolean(1));
-console.log(Boolean(false));
-console.log(Boolean({}));
-console.log(Boolean(undefined));
-
-
-const money = 500;
-
-if (money) {
-  console.log(`The person has ${money} moneys`);
-} else {
-  console.log(`The person has no money`);
-}
-
-
-const age = 11;
-
-if (age === 18) {
-  console.log("you just became an adult");
-} else if (age < 18) {
-  console.log('you are not an adult')
-} 
-
-
-
-let favoriteNumber = Number(prompt("What is your favorite number"));
-
-if (favoriteNumber === 23) {
-  console.log(`Indeed ${favoriteNumber} is a cool number`);
-} else if (favoriteNumber !== 23) {
-  console.log(`Indeed ${favoriteNumber} is a cool number too`);
-} else {
-  console.log(`${favoriteNumber} is not 23 or 7`);
-}
-
-
-let age = prompt('How old are you?')
-
-if (age !== 18){
-  console.log('Age is less than 18')
-}
-
-const hasDriversLicense = true;
-
-const hasGoodVision = true;
-
-const isTired = true;
-
-if (hasDriversLicense && hasGoodVision && !isTired) {
-  console.log('You can drive')
-}
-
-// Two gymnastics teams that compete against each other 3 times,
-// the winner with the highest average score wins the trophy
-
-// 1 - Calculate the average score for each team using the data below
-// 2 - Compare the teams average scores to determine the winner of the competition and print it to the console.
-// dont forget that there can be a draw so test for that as well
-
-// DATA: Dolphins score 96, 108 and 89
-// Koalas score 88, 91 and 110
-let dolphinsAverage = (97 + 112 + 80) / 3;
-let koalasAverage = (109 + 95 + 50) / 3;
-
-console.log(dolphinsAverage);
-console.log(koalasAverage);
-
-if (dolphinsAverage > koalasAverage && dolphinsAverage >= 100) {
-  console.log("Dolphins wins!");
-} else if (koalasAverage > dolphinsAverage && koalasAverage >= 100) {
-  console.log("Koalas wins!");
-} else if (
-  dolphinsAverage === koalasAverage &&
-  dolphinsAverage >= 100 &&
-  koalasAverage >= 100
-) {
-  console.log("Draw!");
-} else {
-  console.log("No team wins the trophy");
-}
-
-
-const day = prompt('What day is today')
-
-switch (day) {
-  case "monday":
-    console.log("Plan course structure");
-    console.log("Go to coding meetup");
-    break;
-  case "tuesday":
-    console.log("Prepare theory videos");
-    break;
-  case "wednesday":
-  case "thursday":
-    console.log("Write code examples");
-    break;
-  case "friday":
-    console.log("Record videos");
-    break;
-  case "saturday":
-  case "sunday":
-    console.log("Enjoy the weekend");
-    break;
-  default:
-    console.log(`${day} is not a valid day`)
-    break;
-}
-
-const day2 = prompt('Whay day is today')
-
-if (day2 === 'monday'){
-  console.log("Plan course structure");
-  console.log("Go to coding meetup");  
-} else if (day2 === 'tuesday'){
-  console.log("Prepare theory videos");
-} else if (day2 === 'wednesday' || day2 === 'thursday'){
-  console.log("Write code examples");
-} else if (day2 === 'friday'){
-  console.log("Record videos");
-} else if (day2 === 'saturday' || day2 === 'sunday'){
-  console.log('Enjoy the weekend')
-} else{
-  console.log(`${day2} is not a valid day`)
-}
-
-let teste = true && false && !false
-
-console.log(teste)
-
-if (23 > 10) {
-  const str = "23 is bigger";
-}
-
-const age = 21;
-
-age >= 18
-  ? console.log("I can drink wine 🍷")
-  : console.log("I can drink water 💧");
-
-const nome = 'jacques'
-const age2 = 11;
-
-const iCanDrink = age2 >= 18 ? 'wine 🍷' : 'water 💧'
-
-console.log(`I can drink ${iCanDrink}`)
-
-console.log(`I can drink ${age2 >= 18 ? 'wine 🍷' : 'water 💧'}`)
-
-console.log(`His name is ${nome === 'jacques' ? 'jacques' : 'Jacão'}`)
-
-const bill = 40;
-const tip = bill > 50 && bill < 301 ? bill * 0.15 : bill * 0.2;
-const billPlusTip = bill + tip;
-
-console.log(
-  `The bill was ${bill}, the tip was ${tip} and the total value is ${billPlusTip}`
-);
-
-let hasDriversLicense = false;
-
-const passTest = false;
-
-switch (passTest) {
-  case true:
-    hasDriversLicense = true;
-    console.log("You now have a drivers license");
-    break;
-  default:
-    console.log("You cant drive yet");
-}
-
-if (passTest) {
-  hasDriversLicense = true;
-}
-
-
-let hasDriversLicense = false;
-
-let passTest = true;
-
-if (passTest) {
-  hasDriversLicense = true;
-}
-
-
-if (hasDriversLicense){
-  console.log('I can drive')
-}*/
-
-class Pokemon {
-  constructor() {
-    this.sprite = null;
-    this.type = null;
+// // console.log(typeof inputYear);
+
+// // let inputNumber = "2002";
+
+// // inputNumber = Number(inputNumber);
+
+// // console.log(20223 - inputNumber);
+
+// // console.log(Boolean(0));
+// // console.log(Boolean(9));
+// // console.log(Boolean("jacques"));
+// // console.log(Boolean(true));
+// // console.log(Boolean(1));
+// // console.log(Boolean(false));
+// // console.log(Boolean({}));
+// // console.log(Boolean(undefined));
+
+// // const money = 500;
+
+// // if (money) {
+// //   console.log(`The person has ${money} moneys`);
+// // } else {
+// //   console.log(`The person has no money`);
+// // }
+
+// // const age = 11;
+
+// // if (age === 18) {
+// //   console.log("you just became an adult");
+// // } else if (age < 18) {
+// //   console.log('you are not an adult')
+// // }
+
+// // let favoriteNumber = Number(prompt("What is your favorite number"));
+
+// // if (favoriteNumber === 23) {
+// //   console.log(`Indeed ${favoriteNumber} is a cool number`);
+// // } else if (favoriteNumber !== 23) {
+// //   console.log(`Indeed ${favoriteNumber} is a cool number too`);
+// // } else {
+// //   console.log(`${favoriteNumber} is not 23 or 7`);
+// // }
+
+// // let age = prompt('How old are you?')
+
+// // if (age !== 18){
+// //   console.log('Age is less than 18')
+// // }
+
+// // const hasDriversLicense = true;
+
+// // const hasGoodVision = true;
+
+// // const isTired = true;
+
+// // if (hasDriversLicense && hasGoodVision && !isTired) {
+// //   console.log('You can drive')
+// // }
+
+// // // Two gymnastics teams that compete against each other 3 times,
+// // // the winner with the highest average score wins the trophy
+
+// // // 1 - Calculate the average score for each team using the data below
+// // // 2 - Compare the teams average scores to determine the winner of the competition and print it to the console.
+// // // dont forget that there can be a draw so test for that as well
+
+// // // DATA: Dolphins score 96, 108 and 89
+// // // Koalas score 88, 91 and 110
+// // let dolphinsAverage = (97 + 112 + 80) / 3;
+// // let koalasAverage = (109 + 95 + 50) / 3;
+
+// // console.log(dolphinsAverage);
+// // console.log(koalasAverage);
+
+// // if (dolphinsAverage > koalasAverage && dolphinsAverage >= 100) {
+// //   console.log("Dolphins wins!");
+// // } else if (koalasAverage > dolphinsAverage && koalasAverage >= 100) {
+// //   console.log("Koalas wins!");
+// // } else if (
+// //   dolphinsAverage === koalasAverage &&
+// //   dolphinsAverage >= 100 &&
+// //   koalasAverage >= 100
+// // ) {
+// //   console.log("Draw!");
+// // } else {
+// //   console.log("No team wins the trophy");
+// // }
+
+// // const day = prompt('What day is today')
+
+// // switch (day) {
+// //   case "monday":
+// //     console.log("Plan course structure");
+// //     console.log("Go to coding meetup");
+// //     break;
+// //   case "tuesday":
+// //     console.log("Prepare theory videos");
+// //     break;
+// //   case "wednesday":
+// //   case "thursday":
+// //     console.log("Write code examples");
+// //     break;
+// //   case "friday":
+// //     console.log("Record videos");
+// //     break;
+// //   case "saturday":
+// //   case "sunday":
+// //     console.log("Enjoy the weekend");
+// //     break;
+// //   default:
+// //     console.log(`${day} is not a valid day`)
+// //     break;
+// // }
+
+// // const day2 = prompt('Whay day is today')
+
+// // if (day2 === 'monday'){
+// //   console.log("Plan course structure");
+// //   console.log("Go to coding meetup");
+// // } else if (day2 === 'tuesday'){
+// //   console.log("Prepare theory videos");
+// // } else if (day2 === 'wednesday' || day2 === 'thursday'){
+// //   console.log("Write code examples");
+// // } else if (day2 === 'friday'){
+// //   console.log("Record videos");
+// // } else if (day2 === 'saturday' || day2 === 'sunday'){
+// //   console.log('Enjoy the weekend')
+// // } else{
+// //   console.log(`${day2} is not a valid day`)
+// // }
+
+// // let teste = true && false && !false
+
+// // console.log(teste)
+
+// // if (23 > 10) {
+// //   const str = "23 is bigger";
+// // }
+
+// // const age = 21;
+
+// // age >= 18
+// //   ? console.log("I can drink wine 🍷")
+// //   : console.log("I can drink water 💧");
+
+// // const nome = 'jacques'
+// // const age2 = 11;
+
+// // const iCanDrink = age2 >= 18 ? 'wine 🍷' : 'water 💧'
+
+// // console.log(`I can drink ${iCanDrink}`)
+
+// // console.log(`I can drink ${age2 >= 18 ? 'wine 🍷' : 'water 💧'}`)
+
+// // console.log(`His name is ${nome === 'jacques' ? 'jacques' : 'Jacão'}`)
+
+// // const bill = 40;
+// // const tip = bill > 50 && bill < 301 ? bill * 0.15 : bill * 0.2;
+// // const billPlusTip = bill + tip;
+
+// // console.log(
+// //   `The bill was ${bill}, the tip was ${tip} and the total value is ${billPlusTip}`
+// // );
+
+// // let hasDriversLicense = false;
+
+// // const passTest = false;
+
+// // switch (passTest) {
+// //   case true:
+// //     hasDriversLicense = true;
+// //     console.log("You now have a drivers license");
+// //     break;
+// //   default:
+// //     console.log("You cant drive yet");
+// // }
+
+// // if (passTest) {
+// //   hasDriversLicense = true;
+// // }
+
+// // let hasDriversLicense = false;
+
+// // let passTest = true;
+
+// // if (passTest) {
+// //   hasDriversLicense = true;
+// // }
+
+// // if (hasDriversLicense){
+// //   console.log('I can drive')
+// // }
+
+// // class Pokemon {
+// //   constructor() {
+// //     this.sprite = null;
+// //     this.type = null;
+// //   }
+// // }
+
+// // let pokemon = prompt("Pokemon name:");
+// // const colours = {
+// // 	normal: '#A8A77A',
+// // 	fire: '#EE8130',
+// // 	water: '#6390F0',
+// // 	electric: '#F7D02C',
+// // 	grass: '#7AC74C',
+// // 	ice: '#96D9D6',
+// // 	fighting: '#C22E28',
+// // 	poison: '#A33EA1',
+// // 	ground: '#E2BF65',
+// // 	flying: '#A98FF3',
+// // 	psychic: '#F95587',
+// // 	bug: '#A6B91A',
+// // 	rock: '#B6A136',
+// // 	ghost: '#735797',
+// // 	dragon: '#6F35FC',
+// // 	dark: '#705746',
+// // 	steel: '#B7B7CE',
+// // 	fairy: '#D685AD',
+// // };
+// // pokemon = pokemon.toLowerCase();
+
+// // function capitalize(str) {
+// //   return str.charAt(0).toUpperCase() + str.slice(1);
+// // }
+
+// // const myPokemon = new Pokemon();
+
+// // fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+// //   .then((response) => {
+// //     console.log(response.status);
+// //     return response.json();
+// //   })
+// //   .then((data) => {
+// //     myPokemon.sprite = data.sprites.other.dream_world.front_default;
+// //     myPokemon.type = data.types;
+// //     document.getElementById("pokemon-image").src = myPokemon.sprite;
+// //     for (let i = 0; i < myPokemon.type.length; i++) {
+// //       const typeElement = document.createElement("h1");
+// //       typeElement.textContent = capitalize(myPokemon.type[i].type.name);
+// //       let color = myPokemon.type[i].type.name
+// //       typeElement.style.color = colours[color]
+// //       poketypes.appendChild(typeElement);
+// //     }
+// //     document.getElementById("pokename").textContent = capitalize(pokemon);
+// //   })
+// //   .catch((error) => {
+// //     console.error(error);
+// //     document.getElementById("error").textContent =
+// //       "Could not find that Pokémon";
+// //   });
+
+// // function logger(name, times){
+// //   for (i = 0; i < times; i++){
+// //     console.log(`My name is ${name}`)
+// //   }
+// // }
+// // logger('jacques', 10)
+
+// // function fruitProcessor(apples, oranges){
+// //  const juice = `Juice made with ${apples} apples and ${oranges} oranges `
+// //  return juice
+// // }
+
+// // console.log(fruitProcessor(5,5))
+
+// // let calcAge = function (birthYear) {
+// //   return 2023 - birthYear;
+// // };
+
+// // year = 2002;
+
+// // console.log(calcAge(year));
+
+// const friends = ["Jacques", "jacão", "jaca", "junin"];
+
+// console.log(friends[friends.length - 1]);
+// console.log(years[years.length - 1]);
+// console.log(years);
+// years[years.length -1] = 2023
+// console.log(years)
+
+const years = new Array(1991, 2002, 2001, 1945);
+
+const jacques = ["Jacques", "Drumond", 2023 - 2002, "Programmer"];
+
+const showArrayElements = (Array) => {
+  for (i = 0; i < Array.length; i++) {
+    console.log(Array[i]);
   }
-}
-
-let pokemon = prompt("Pokemon name:");
-const colours = {
-	normal: '#A8A77A',
-	fire: '#EE8130',
-	water: '#6390F0',
-	electric: '#F7D02C',
-	grass: '#7AC74C',
-	ice: '#96D9D6',
-	fighting: '#C22E28',
-	poison: '#A33EA1',
-	ground: '#E2BF65',
-	flying: '#A98FF3',
-	psychic: '#F95587',
-	bug: '#A6B91A',
-	rock: '#B6A136',
-	ghost: '#735797',
-	dragon: '#6F35FC',
-	dark: '#705746',
-	steel: '#B7B7CE',
-	fairy: '#D685AD',
 };
-pokemon = pokemon.toLowerCase();
 
-function capitalize(str) {
-  return str.charAt(0).toUpperCase() + str.slice(1);
-}
+const reverseArray = (arrayToReverse) => {
+  const reversedArray = new Array();
+  for (i = arrayToReverse.length - 1; i > -1; i--) {
+    reversedArray.push(arrayToReverse[i]);
+  }
+  return reversedArray;
+};
 
-const myPokemon = new Pokemon();
+let ReversedArray1 = reverseArray(years);
+let ReversedArray2 = reverseArray(jacques);
 
-fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
-  .then((response) => {
-    console.log(response.status);
-    return response.json();
-  })
-  .then((data) => {
-    myPokemon.sprite = data.sprites.other.dream_world.front_default;
-    myPokemon.type = data.types;
-    document.getElementById("pokemon-image").src = myPokemon.sprite;
-    for (let i = 0; i < myPokemon.type.length; i++) {
-      const typeElement = document.createElement("h1");
-      typeElement.textContent = capitalize(myPokemon.type[i].type.name);
-      let color = myPokemon.type[i].type.name
-      typeElement.style.color = colours[color]
-      poketypes.appendChild(typeElement);
-    }
-    document.getElementById("pokename").textContent = capitalize(pokemon);
-  })
-  .catch((error) => {
-    console.error(error);
-    document.getElementById("error").textContent =
-      "Could not find that Pokémon";
-  });
+console.log(ReversedArray1)
+console.log(ReversedArray2)
